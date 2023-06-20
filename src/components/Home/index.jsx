2cee48a7c63efe3872ac823ffd149e523d840bfb
Victor Gonzalez <vitorpcg@gmail.com>
2023-06-20 17:44:59 +0100

    handleNextTab
    included final logic into handleNextTab

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,14 +2,22 @@ import { Sector } from "../Sector";
 import "./index.css";
 
 // eslint-disable-next-line react/prop-types
-export function Home({handleTabClick, activeTab}) {
+export function Home({ handleTabClick, activeTab }) {
+  
   const handleNextTab = () => {
     if (activeTab === "Sector") {
       handleTabClick("Tipo de empresa");
     } else if (activeTab === "Tipo de empresa") {
       handleTabClick("Espacio");
+    } else if (activeTab === "Espacio") {
+      handleTabClick("Personas");
+    } else if (activeTab === "Personas") {
+      handleTabClick("Productos");
+    } else if (activeTab === "Productos") {
+      handleTabClick("Resultados");
     }
   };
+
  
   return (
     <>
