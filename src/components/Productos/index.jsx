79e2a9fadb6bf022b0336aa936bb397366e428f5
Victor Gonzalez <vitorpcg@gmail.com>
2023-06-27 14:46:17 +0100

    Productos
    added radio Inputs to "Productos"

--- src/components/Productos/index.jsx
+++ src/components/Productos/index.jsx
@@ -7,7 +7,24 @@ export function Productos() {
       role="tabpanel"
       aria-labelledby="pills-productos-tab"
     >
-      <p>¿Como te gustaria tener tu espacio?</p>
+      <p>¿Como te gustaria tener tus productos?</p>
+      <fieldset>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                checked />
+          <label for="huey">Sofisticados</label>
+        </div>
+
+        <div>
+          <input type="radio" id="dewey" name="drone" value="dewey" />
+          <label for="dewey">Modernos</label>
+        </div>
+
+        <div>
+          <input type="radio" id="louie" name="drone" value="louie" />
+          <label for="louie">Antiguos con agregado valor</label>
+        </div>
+      </fieldset>
     </div>
   );
 }
