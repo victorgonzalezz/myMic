8a6224fe8871ed24fb9f3c5b5e60a5a3961f8d76
Victor Gonzalez <vitorpcg@gmail.com>
2023-06-27 14:36:27 +0100

    Input
    Added radio Inputs to "Tipo De Empresa"

--- src/components/TipoDeEmpresa/index.jsx
+++ src/components/TipoDeEmpresa/index.jsx
@@ -7,6 +7,23 @@ export function TipoDeEmpresa() {
       aria-labelledby="pills-tipo-empresa-tab"
     >
       <p>¿Cuál tipo de negocio describe su empresa?</p>
+      <fieldset>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                checked />
+          <label for="huey">Buena</label>
+        </div>
+
+        <div>
+          <input type="radio" id="dewey" name="drone" value="dewey" />
+          <label for="dewey">Muy Buena</label>
+        </div>
+
+        <div>
+          <input type="radio" id="louie" name="drone" value="louie" />
+          <label for="louie">Excelente</label>
+        </div>
+      </fieldset>
     </div>
   );
 }
